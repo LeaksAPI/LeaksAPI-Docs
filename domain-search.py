bae442ea7a747542cb1fc219e055f3ef3ef22dd0
LeaksAPI <mg4777@proton.me>
2025-07-03 23:44:40 -0500

    improved domain-search.py

--- domain-search.py
+++ domain-search.py
@@ -1,15 +1,34 @@
 import http.client
+import json
+import sys
 
+# Check that a domain was provided
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <domain>")
+    sys.exit(1)
+
+# Get the domain from command-line arguments
+domain = sys.argv[1]
+
+# Set up HTTPS connection
 conn = http.client.HTTPSConnection("leaksapi.p.rapidapi.com")
 
+# Set request headers with your API key and host
 headers = {
-    'x-rapidapi-key': "YOUR KEY GOES HERE",
+    'x-rapidapi-key': "YOUR KEY HERE",
     'x-rapidapi-host': "leaksapi.p.rapidapi.com"
 }
 
-conn.request("GET", "/api/v2/query/pizza.co.uk?type=domain", headers=headers)
+# Make the GET request to the API for the specified domain
+endpoint = f"/api/v2/query/{domain}?type=domain"
+conn.request("GET", endpoint, headers=headers)
 
+# Get and read the response
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+# Decode the response data and parse as JSON
+parsed_json = json.loads(data.decode("utf-8"))
+
+# Pretty print the parsed JSON with indentation
+print(json.dumps(parsed_json, indent=2))
